9e43e62e179370c0a7f268c2742cd330ef78bec8
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-08-09 15:16:07 -0500

    feat:se implementa login component

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
-import { InicioComponent } from "./components/inicio/inicio.component";;
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const appRoute: Routes = [
 //rutas de nuestro poryecto en angular
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
-    {path:'inicio', component: InicioComponent }
+    {path:'inicio', component: InicioComponent },
+    {path:'login', component: LoginComponent }
 ]
 
 export const appRoutingPorviders : any[]=[];
