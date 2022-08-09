88b9ac9662859b6859a16b5eb2bdba144fcd716e
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-08-09 15:50:21 -0500

    feat:se implementa clientes modulo

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -2,11 +2,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { LoginComponent } from "./components/login/login.component";
+import { IndexClienteComponent } from "./components/clientes/index-cliente/index-cliente.component";
 
 const appRoute: Routes = [
 //rutas de nuestro poryecto en angular
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
     {path:'inicio', component: InicioComponent },
+    {path:'panel', children: [
+       {path:'clientes', component: IndexClienteComponent },
+    ]},
     {path:'login', component: LoginComponent }
 ]
 
--- admin/src/app/services/GLOBAL.ts
+++ admin/src/app/services/GLOBAL.ts
@@ -1,3 +1,3 @@
 export var GLOBAL = {
-    url: 'http://localhost:4202/api/',
+    url: 'http://localhost:42003/api/',
 }
